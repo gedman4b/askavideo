a7cff8184508efabd477d5bc18afaff920de057c
Scott Josephson <gedman4b@yahoo.com>
2024-10-23 05:59:11 +0000

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,26 @@
 import streamlit as st
 from openai import OpenAI
+import transcript_RAG as tr
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Ask Your Youtube Video")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot that you load a youtube url and then ask it a question about the video."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+url = st.text_input("Youtube URL")
+
+tr.load_transcript(url)
+
+if not UnicodeTranslateError:
+     st.info("Please enter a Youtube URL to continue.")
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -32,7 +34,7 @@ else:
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("Ask your question"):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
@@ -41,16 +43,23 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4o-mini",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
+            documents = convo_qa_chain.invoke(
+    {
+        "input": "What are autonomous agents?",
+        "chat_history": [],
+    }
+)
             stream=True,
         )
-
+        
         # Stream the response to the chat using `st.write_stream`, then store it in 
         # session state.
         with st.chat_message("assistant"):
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
+        
---
+++ transcript_RAG.py
@@ -0,0 +1,60 @@
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.document_loaders import YoutubeLoader
+
+def load_transcript(url):
+    #video = 'https://www.youtube.com/watch?v=9vM4p9NN0Ts'
+    loader = YoutubeLoader(url.split('v=')[1])
+    transcript = loader.load()
+    return transcript
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
+docs_split = text_splitter.split_documents(transcript)
+
+db = DocArrayInMemorySearch.from_documents(docs_split,embedding=OpenAIEmbeddings())
+retriever = db.as_retriever()
+llm = ChatOpenAI(temperature=0,model_name='gpt-4o-mini')
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+condense_question_system_template = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+
+condense_question_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", condense_question_system_template),
+        ("placeholder", "{chat_history}"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, condense_question_prompt
+)
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("placeholder", "{chat_history}"),
+        ("human", "{input}"),
+    ]
+)
+qa_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+convo_qa_chain = create_retrieval_chain(history_aware_retriever, qa_chain)
